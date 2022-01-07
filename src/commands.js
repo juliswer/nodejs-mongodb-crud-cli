b078807fc69dd024b6a870c935db49bfad8d6c5a
Julian Swerdlin <julianswer36@gmail.com>
2022-01-07 22:43:59 +0100

    adapting inquirer

--- src/commands.js
+++ src/commands.js
@@ -1,9 +1,23 @@
 const {program} = require('commander');
+const {prompt} = require('inquirer');
 
 program.version('0.0.1').description('A command line tool for managing your projects');
 
-program.command('save <title> <description>').action((title, description) => {
-    console.log(title, description);
+program.command('save').action(async () => {
+    const answers = await prompt([
+        {
+            type: 'input',
+            message: 'Task Title',
+            name: 'title'
+        },
+        {
+            type: 'input',
+            message: 'Task Description',
+            name: 'description'
+        }
+    ]);
+
+    console.log(answers);
 });
 
 program.parse(process.argv);
