dfc83191787eac839f55ec219dd19981ac352366
Julian Swerdlin <julianswer36@gmail.com>
2022-01-07 22:58:46 +0100

    mongodb connection

--- src/controllers/task.controllers.js
+++ src/controllers/task.controllers.js
@@ -1,7 +1,9 @@
 const Task = require('../models/Task');
+const {connection} = require('../db')
 
 const addTask = async (task) => {
     await Task.create(task);
+    await connection.close();
 }
 
 module.exports = {
--- src/db.js
+++ src/db.js
@@ -1,10 +1,13 @@
-const {connect} = require('mongoose');
+const {connect, connection} = require('mongoose');
 
 const connectDB = async () => {
     await connect('mongodb://localhost/taskcli')
     // console.log('MongoDB Connected')
 }
 
+connection.on('error', err => console.log(err))
+
 module.exports= {
-    connectDB
+    connectDB,
+    connection
 }
