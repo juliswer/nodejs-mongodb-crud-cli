bccd9121b40afc6e15072ef648ee7da24d7d8770
Julian Swerdlin <julianswer36@gmail.com>
2022-01-07 22:37:53 +0100

    uploading files

--- src/commands.js
+++ src/commands.js
@@ -2,4 +2,8 @@ const {program} = require('commander');
 
 program.version('0.0.1').description('A command line tool for managing your projects');
 
+program.command('save').action(() => {
+    console.log('guardando')
+});
+
 program.parse(process.argv);
